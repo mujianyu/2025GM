40cf56a3dc67b9a43fa445594cd0ed7ae1a86c96
Mother Jianyu <2559405752@qq.com>
2025-02-05 12:55:02 +0800

    记录加回溯

---
+++ Assets/Scripts/PlayerMove/Replay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Replay : MonoBehaviour
+{
+    private Button StartRecord;
+    
+    private bool statr = false;
+    public RercordAction Recordaction;
+    public ActionBase actionBase;
+    private void Start()
+    {
+        StartRecord = GetComponent<Button>();
+        StartRecord.onClick.AddListener(StartR);
+    }
+    private void StartR()
+    {
+        if (statr)
+        {
+            statr = false;
+            Recordaction.RecordStart = false;
+        }
+        else
+        {
+            statr = true;
+            actionBase.clear();
+            Recordaction.RecordStart = true;
+        }
+    }
+
+}
